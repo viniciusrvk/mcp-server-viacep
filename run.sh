96c91e5f20adb4317758e5db3509d4fe9457f8c2
Vinicius Kuo <roberto.kuo@luizalabs.com>
2025-07-26 15:23:44 -0300

    ci: executavel run para deploy

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ajustando diretório do arquivo app.jar para funcionar de acordo com o DockerFile do ambiente coolify
+JAR_FILE="./app.jar" # O JAR agora está diretamente em /app, renomeado para app.jar
+
+# Verifica se o arquivo JAR foi encontrado.
+# O `find` não é mais necessário, apenas verificar se o arquivo existe.
+if [ ! -f "$JAR_FILE" ]; then
+    echo "Erro: Arquivo JAR não encontrado em '$JAR_FILE'."
+    exit 1
+fi
+
+echo "Executando a aplicação: $JAR_FILE"
+# Executa o JAR da aplicação Spring Boot.
+java -jar -Dspring.profiles.active=develop "$JAR_FILE"
