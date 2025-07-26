7e13716d9f8005808145f68da935108b62b322dc
Vinicius Kuo <roberto.kuo@luizalabs.com>
2025-07-26 15:09:41 -0300

    feat: poc mcp sse viacep

--- src/main/java/com/viniciusrvk/mcp_server_viacep/service/CepService.java
+++ src/main/java/com/viniciusrvk/mcp_server_viacep/service/CepService.java
@@ -4,6 +4,7 @@ import com.viniciusrvk.mcp_server_viacep.client.ViaCepClient;
 import com.viniciusrvk.mcp_server_viacep.dto.CepResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class CepService {
     private final ViaCepClient viaCepClient;
 
     @Tool(description = "Consulta de endereço via CEP")
-    public CepResponse getCep(final String cep){
+    public CepResponse getCep(@ToolParam(description = "CEP, e.g., 59154310") String cep){
         final var cepTratado = cep.replaceAll("[^0-9]", "");
         return viaCepClient.addressByCep(cepTratado).block();
     }
